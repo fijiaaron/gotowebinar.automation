f51a2cc1d778d15aa31b7bae9d13f54d5b3ebd66
Aaron Evans <fijiaaron@one-shore.com>
2015-11-21 01:50:13 -0700

    more prominent error message

--- src/main/java/citrix/gotowebinar/automation/WebDriverTestCase.java
+++ src/main/java/citrix/gotowebinar/automation/WebDriverTestCase.java
@@ -27,9 +27,12 @@ public class WebDriverTestCase {
         try {
             properties = PropertyLoader.LoadProperties(propertiesFile);
         } catch (Exception e) {
-            System.out.println("*******");
-            System.out.println("You probably need to configure a test.properties file.  See the example under src/main/resources");
-            System.out.println("*******");
+            System.out.println();
+            System.out.println("******************************************************");
+            System.out.println("You probably need to configure a test.properties file.");
+            System.out.println("See the example under src/main/resources");
+            System.out.println("******************************************************");
+            System.out.println();
         }
     }
 
